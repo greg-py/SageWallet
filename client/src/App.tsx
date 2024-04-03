5aabc21426af3936ea458c7535b9bef3f1137a81
greg-py <gking@performyard.com>
2024-04-03 12:49:02 -0400

    Added auth0 integration into client

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,24 @@
+import LoginButton from "./components/auth/LoginButton";
+import LogoutButton from "./components/auth/LogoutButton";
+import Profile from "./components/auth/Profile";
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <div>
+        <h5>Login</h5>
+        <LoginButton />
+      </div>
+      <div>
+        <h5>Logout</h5>
+        <LogoutButton />
+      </div>
+      <div>
+        <h5>Profile</h5>
+        <Profile />
+      </div>
+    </div>
+  );
 };
 
 export default App;
